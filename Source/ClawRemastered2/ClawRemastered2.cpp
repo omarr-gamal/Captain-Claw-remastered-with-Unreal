cec7f7d9cbfb80b8de9b1d237260082ea8a46547
Mariam Isaac <marygate2002@gmail.com>
2021-05-22 00:32:05 +0200

    yes

--- Source/ClawRemastered2/ClawRemastered2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ClawRemastered2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClawRemastered2, "ClawRemastered2" );
