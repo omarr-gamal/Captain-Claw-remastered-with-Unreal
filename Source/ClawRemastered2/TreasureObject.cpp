6f1db780e734a7b8efcc521efe66c29797eb041c
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-05-26 13:55:11 +0200

    created TreasureObject class

---
+++ Source/ClawRemastered2/TreasureObject.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TreasureObject.h"
+#include "PaperFlipbookComponent.h" 
+#include "Components/CapsuleComponent.h"  
+#include "Components/BoxComponent.h"
+#include "ClawRemastered2Character.h"
+#include "Kismet/GameplayStatics.h"	
+#include "Components/CapsuleComponent.h"
+#include "Engine/Engine.h"
+
+ATreasureObject::ATreasureObject()
+{
+	// initializes the enemy's box collision 
+	attackCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("AttackCollision"));
+	attackCollisionBox->SetBoxExtent(FVector(32.0f, 32.0f, 32.0f));
+	attackCollisionBox->SetCollisionProfileName("Trigger");
+	attackCollisionBox->SetupAttachment(RootComponent);
+
+	attackCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ATreasureObject::OnOverlapBegin);
+	attackCollisionBox->OnComponentEndOverlap.AddDynamic(this, &ATreasureObject::OnOverlapEnd);
+}
+
+void ATreasureObject::BeginPlay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("started treasure"));
+}
+
+void ATreasureObject::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ATreasureObject::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
---
+++ Source/ClawRemastered2/TreasureObject.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "TreasureObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLAWREMASTERED2_API ATreasureObject : public APaperFlipbookActor
+{
+	GENERATED_BODY()
+	
+	virtual void BeginPlay();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	class UPaperFlipbook* BaseAnimation;
+
+public:
+	ATreasureObject();
+
+private:
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* attackCollisionBox;
+};
