2f6f578a9f3ee7dd44851e9c3dc17ff96f1760d2
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2022-04-14 13:48:28 +0200

    refactored health component to call OnDamageTaken interface function of owner actor

---
+++ Source/ClawRemastered2/PinkOfficer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "PinkOfficer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLAWREMASTERED2_API APinkOfficer : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
